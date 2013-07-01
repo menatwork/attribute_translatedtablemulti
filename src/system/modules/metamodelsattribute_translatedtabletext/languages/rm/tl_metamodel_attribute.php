17abda19704080c57683d43e1c41ebe56050d9bc
Mini Model <minimodel@metamodel.me>
2013-07-01 04:16:46 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_translatedtabletext/languages/rm/tl_metamodel_attribute.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-07-01T00:11:52+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']     = 'Configuraziun da las colonnas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0'] = 'Label';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0'] = 'Ladezza';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1'] = 'Endatescha la ladezza da las colonnas (p.ex. 200px)';
